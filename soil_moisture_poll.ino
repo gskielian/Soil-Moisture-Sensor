26776eba35ad73f80ce705a561972c63002b87dc
gskielian <gregory.kielian@gmail.com>
2014-01-16 01:16:05 -0800

    allowed the python script to do most of the work

--- soil_moisture_poll.ino
+++ soil_moisture_poll.ino
@@ -1,22 +1,32 @@
+char ch;
 void setup() {
-
   Serial.begin(115200);
-
 }
 
 
 void loop() {
-  forward_measurement();
-  reverse_measurement();
+
+  if (Serial.available()) {
+    ch = Serial.read();
+    if (ch == '1') {
+      forward_measurement();
+    } else if (ch == '2') {
+      reverse_measurement();
+    } else {
+      delay(1);
+    }
+  }
+
 }
 
 
 
-void forward_measurement() 
-{
 
-Serial.println("testing pre measurement");
-Serial.println(analogRead(1));
+void forward_measurement() {
+
+//this was here for debugging
+//Serial.println("testing pre measurement");
+//Serial.println(analogRead(1));
 
 //FIRST we make the voltage divider
 
@@ -32,8 +42,7 @@ digitalWrite(18,LOW);
 
 //NEXT we poll for voltage reading
 delay(500);
-Serial.println("measurement stabilizing");
-Serial.println(analogRead(1));
+Serial.print("Forward Measurement, "); Serial.println(analogRead(1));
 
 //LAST we set all pins to 0V to remove hysterisis
 
@@ -41,43 +50,37 @@ digitalWrite(14,LOW);
 pinMode(16,OUTPUT);
 digitalWrite(16,LOW);
 
-//this delay allows the charge to fully drain to gnd (should be fairly rapid
-
-delay(1000);
+//will do in python a 1 second delay here
 
 }
 
-void reverse_measurement()
-{
 
-Serial.println("testing pre measurement");
-Serial.println(analogRead(1));
+void reverse_measurement() {
 
-//FIRST we make the voltage divider
+  Serial.println("testing pre measurement");
+  Serial.println(analogRead(1));
 
-//16 to be an input (not really necessary the first time)
-pinMode(16, INPUT);
+  //FIRST we make the voltage divider
 
-//we set 14 and 18 (aka a0 and a4) to outputs
-pinMode(14, OUTPUT);
-digitalWrite(14,LOW);
+  //16 to be an input (not really necessary the first time)
+  pinMode(16, INPUT);
 
-pinMode(18, OUTPUT);
-digitalWrite(18,HIGH);
+  //we set 14 and 18 (aka a0 and a4) to outputs
+  pinMode(14, OUTPUT);
+  digitalWrite(14,LOW);
 
-//NEXT we poll for voltage reading
-delay(500);
-Serial.println("measurement stabilizing");
-Serial.println(1023-analogRead(1));
+  pinMode(18, OUTPUT);
+  digitalWrite(18,HIGH);
 
-//LAST we set all pins to 0V to remove hysterisis
+  //NEXT we poll for voltage reading
+  delay(500);
+  Serial.print("Reverse_Measurement"); Serial.println(1023-analogRead(1));
 
-digitalWrite(18,LOW);
-pinMode(16,OUTPUT);
-digitalWrite(16,LOW);
+  //LAST we set all pins to 0V to remove hysterisis
 
-//this delay allows the charge to fully drain to gnd (should be fairly rapid
+  digitalWrite(18,LOW);
+  pinMode(16,OUTPUT);
+  digitalWrite(16,LOW);
 
-delay(1000);
-
+  //again a python delay of one second goes after this
 }
