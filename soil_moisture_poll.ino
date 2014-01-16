527939fd837d13b43bc89a58c20afaea13a85958
gskielian <gregory.kielian@gmail.com>
2014-01-16 01:02:53 -0800

    added reverse and forwards measurement
    it seems that adding forward and reverse
    measurement helps.
    
    There might be some hysterisis in the water
    in addition to hysterisis in the accumulated
    charge.

--- soil_moisture_poll.ino
+++ soil_moisture_poll.ino
@@ -6,35 +6,78 @@ void setup() {
 
 
 void loop() {
+  forward_measurement();
+  reverse_measurement();
+}
 
-  Serial.println("testing pre measurement");
-  Serial.println(analogRead(1));
+
+
+void forward_measurement() 
+{
+
+Serial.println("testing pre measurement");
+Serial.println(analogRead(1));
 
 //FIRST we make the voltage divider
 
-  //16 to be an input (not really necessary the first time)
-  pinMode(16, INPUT);
+//16 to be an input (not really necessary the first time)
+pinMode(16, INPUT);
 
-  //we set 14 and 18 (aka a0 and a4) to outputs
-  pinMode(14, OUTPUT);
-  digitalWrite(14,HIGH);
+//we set 14 and 18 (aka a0 and a4) to outputs
+pinMode(14, OUTPUT);
+digitalWrite(14,HIGH);
 
-  pinMode(18, OUTPUT);
-  digitalWrite(18,LOW);
+pinMode(18, OUTPUT);
+digitalWrite(18,LOW);
 
 //NEXT we poll for voltage reading
-  delay(500);
-  Serial.println("measurement stabilizing");
-  Serial.println(analogRead(1));
+delay(500);
+Serial.println("measurement stabilizing");
+Serial.println(analogRead(1));
 
 //LAST we set all pins to 0V to remove hysterisis
 
-  digitalWrite(14,LOW);
-  pinMode(16,OUTPUT);
-  digitalWrite(16,LOW);
+digitalWrite(14,LOW);
+pinMode(16,OUTPUT);
+digitalWrite(16,LOW);
 
 //this delay allows the charge to fully drain to gnd (should be fairly rapid
 
-  delay(1000);
+delay(1000);
+
+}
+
+void reverse_measurement()
+{
+
+Serial.println("testing pre measurement");
+Serial.println(analogRead(1));
+
+//FIRST we make the voltage divider
+
+//16 to be an input (not really necessary the first time)
+pinMode(16, INPUT);
+
+//we set 14 and 18 (aka a0 and a4) to outputs
+pinMode(14, OUTPUT);
+digitalWrite(14,LOW);
+
+pinMode(18, OUTPUT);
+digitalWrite(18,HIGH);
+
+//NEXT we poll for voltage reading
+delay(500);
+Serial.println("measurement stabilizing");
+Serial.println(1023-analogRead(1));
+
+//LAST we set all pins to 0V to remove hysterisis
+
+digitalWrite(18,LOW);
+pinMode(16,OUTPUT);
+digitalWrite(16,LOW);
+
+//this delay allows the charge to fully drain to gnd (should be fairly rapid
+
+delay(1000);
 
 }
