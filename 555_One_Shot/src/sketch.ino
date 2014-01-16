b78c2c47513f35576cea33b24768b1946904c69b
gskielian <gregory.kielian@gmail.com>
2014-01-16 04:05:44 -0800

    added a lot of work on one shot -- please help!
    one shot is not working, it seems that
    I will need to set the pulseIn before the
    timer goes off.
    
    On the other hand I know that the one shot
    is going off, as I can easily place an
    led there.
    
    I could probably place another arduino there
    just to see if it would work though

---
+++ 555_One_Shot/src/sketch.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+Servo hack;
+
+char ch;
+unsigned long duration;
+
+void setup()
+{
+  hack.attach(9);
+  pinMode(2,INPUT);
+  digitalWrite(4, HIGH);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (Serial.available()) {
+    ch = Serial.read();
+    if (ch == '1') {
+      //digitalWrite(4, LOW);
+      //digitalWrite(4, HIGH);
+      hack.write(180);
+      duration = pulseIn(2,HIGH);
+      Serial.println(duration);
+      duration = pulseIn(2,HIGH);
+      Serial.println(duration);
+      duration = pulseIn(2,HIGH);
+      Serial.println(duration);
+      delay(3000);
+    }
+    else {
+    delay(1);
+    }
+  }
+}
+
